2ab4f4971c4acd7a765b05d36a11882dc9dad941
keng <simon.keng@schrodinger.com>
2019-06-27 10:43:11 -0700

    Nginx errors are gone

--- api/api.py
+++ api/api.py
@@ -1,5 +1,11 @@
 import hug
+import requests
 
 @hug.get('/greet/{user}', version=1)
 def greet(user: str):
+    
+    # api to access "web" container
+    # r = requests.get('web:8000/cat')
+    # print(r.status_code)
+    
     return f"Hello {user}! Nice to meet you."
